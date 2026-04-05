3e24d454d8bb12faeb7e30fae516b7b8c800493a
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-05 20:29:31 +0200

    edited a few comments

--- contained.c
+++ contained.c
@@ -1,8 +1,3 @@
-/* -*- compile-command: "gcc -Wall -Werror -lcap -lseccomp contained.c -o contained" -*- */
-/* This code is licensed under the GPLv3. You can find its text here:
-   https://www.gnu.org/licenses/gpl-3.0.en.html */
-
-
 #define _GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
